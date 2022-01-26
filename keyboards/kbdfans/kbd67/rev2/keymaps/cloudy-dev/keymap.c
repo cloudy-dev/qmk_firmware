20236bd641d317c0bd1accd4e28fe570f4d6a2b8
cloudy-dev <71619239+cloudy-dev@users.noreply.github.com>
2022-01-26 06:42:51 +0100

    Adjust layers 0 & 1 and comment out backlight rule

--- keyboards/kbdfans/kbd67/rev2/keymaps/cloudy-dev/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/cloudy-dev/keymap.c
@@ -46,43 +46,44 @@ const uint32_t PROGMEM unicode_map[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
+   * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp | ~ `|
    * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |Caps   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
+   * |Caps   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |Home|
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
    * |----------------------------------------------------------------|
-   * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
+   * |Ctrl|Win |Alt |        Space          | FN|Alt|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
 [0] = LAYOUT_65_ansi(
-  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
-  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-  KC_CAPS, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, KC_PGDN,
+  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,
+  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+  KC_CAPS, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, KC_HOME,
   KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
   KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                          MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap Fn Layer
    * ,----------------------------------------------------------------.
-   * |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+   * |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |UCMD|
    * |----------------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |PSc|SLk|Pau|Up |   |     |    |
+   * |     |   |€  |   |   |   |Ü  |   |Ö  |   |7  |8  |9  |PSc  |Ins |
    * |----------------------------------------------------------------|
-   * |      |Lef|Dow|Rig|   |   |   |   |Hom|PUp|Lef|Rig|        |    |
+   * |      |Ä  |ß  |   |   |   |   |   |   |   |4  |5  |6       |PUp |
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |End|PDn|Dow|      |PUp|    |
+   * |        |   |   |   |   |   |   |   |   |1  |2  |3     |PUp|PDn |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |Hom|PDn|End |
+   * |    |    |    |                       |   |0  |NLCK|Hom|PDn|End |
    * `----------------------------------------------------------------'
    */
 [1] = LAYOUT_65_ansi(
    KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,          KC_DEL,UC_MOD,
-   _______,_______,  KC_UP,X(EURO_SIGN),_______,_______,_______,XP(UE_SMALL, UE_BIG),KC_PSCR,XP(OE_SMALL, OE_BIG),KC_PAUS,  KC_UP,_______,     _______,_______,
-   _______,XP(AE_SMALL, AE_BIG),X(SZ_SMALL),KC_RGHT,_______,_______,_______,_______,KC_HOME,KC_PGUP,KC_LEFT,KC_RGHT,            _______,_______,
-           _______,_______,_______,_______,_______,_______,_______,_______, KC_END,KC_PGDN,KC_DOWN,       _______,KC_PGUP,_______,
-    _______,  _______,  _______,                     _______,                     _______,_______,_______,KC_HOME,KC_PGDN, KC_END),
+   _______,_______,  _______,X(EURO_SIGN),_______,_______,_______,XP(UE_SMALL, UE_BIG),_______,XP(OE_SMALL, OE_BIG),KC_P7,  KC_P8,KC_P9,     KC_PSCR,KC_INS,
+   _______,XP(AE_SMALL, AE_BIG),X(SZ_SMALL),_______,_______,_______,_______,_______,_______,_______,KC_P4,KC_P5, KC_P6,KC_PGUP,
+           _______,_______,_______,_______,_______,_______,_______,_______, _______,KC_P1,KC_P2,       KC_P3,KC_PGUP,KC_PGDN,
+    _______,  _______,  _______,                     _______,                     _______,KC_P0,KC_NLCK,KC_HOME,KC_PGDN, KC_END),
+
 
 [2] = LAYOUT_65_ansi(
   _______, _______,   _______, _______,   _______, _______,   _______, _______,   _______, _______,   _______, _______,  _______, _______, _______,
